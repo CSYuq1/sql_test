346b90c81e272f5175c6593eba4c314aa19dc2ab
yanyuqi <1900387331@qq.com>
2023-08-15 17:26:08 +0800

    Changes

---
+++ cmake-build-debug/CMakeFiles/sql_test.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/yanyu/CLionProjects/sql_test/code/Sql_print.cpp" "CMakeFiles/sql_test.dir/code/Sql_print.cpp.obj" "gcc" "CMakeFiles/sql_test.dir/code/Sql_print.cpp.obj.d"
+  "C:/Users/yanyu/CLionProjects/sql_test/code/main.cpp" "CMakeFiles/sql_test.dir/code/main.cpp.obj" "gcc" "CMakeFiles/sql_test.dir/code/main.cpp.obj.d"
+  "C:/Users/yanyu/CLionProjects/sql_test/code/sql_access.cpp" "CMakeFiles/sql_test.dir/code/sql_access.cpp.obj" "gcc" "CMakeFiles/sql_test.dir/code/sql_access.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sql_test.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sql_test.dir/code/Sql_print.cpp.obj"
+  "CMakeFiles/sql_test.dir/code/Sql_print.cpp.obj.d"
+  "CMakeFiles/sql_test.dir/code/main.cpp.obj"
+  "CMakeFiles/sql_test.dir/code/main.cpp.obj.d"
+  "CMakeFiles/sql_test.dir/code/sql_access.cpp.obj"
+  "CMakeFiles/sql_test.dir/code/sql_access.cpp.obj.d"
+  "libsql_test.dll.a"
+  "sql_test.exe"
+  "sql_test.exe.manifest"
+  "sql_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sql_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
